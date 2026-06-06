e59ebaad8483ab5b812fc48ca0d5dbea5f4146e5
riaa52633 <riaa52633@gmail.com>
2026-06-06 22:39:22 +0700

    Mengubah halaman about menjadi profil mahasiswa Ria Adriani

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,42 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Profil Mahasiswa')
 
 @section('content')
-<div class="bg-white p-8 rounded-lg shadow">
-    <h1 class="text-4xl font-bold text-blue-600">
-        Halaman Home
-    </h1>
 
-    <p class="mt-4 text-gray-600">
-        Selamat datang di website Laravel 12.
-    </p>
+<div class="max-w-3xl mx-auto bg-white rounded-xl shadow-lg p-8 mt-10">
+
+    <div class="text-center">
+        <div class="w-24 h-24 mx-auto bg-blue-500 rounded-full flex items-center justify-center text-white text-4xl font-bold">
+            A
+        </div>
+
+        <h1 class="text-3xl font-bold mt-4 text-gray-800">
+            Annisa Nurul Aulia
+        </h1>
+
+        <p class="text-gray-500">
+            Mahasiswa Teknik Informatika
+        </p>
+    </div>
+
+    <div class="mt-8 space-y-4">
+        <div class="flex justify-between border-b pb-2">
+            <span class="font-semibold">Nama</span>
+            <span>Annisa Nurul Aulia</span>
+        </div>
+
+        <div class="flex justify-between border-b pb-2">
+            <span class="font-semibold">NIM</span>
+            <span>F1A250018</span>
+        </div>
+
+        <div class="flex justify-between border-b pb-2">
+            <span class="font-semibold">Program Studi</span>
+            <span>Teknik Informatika</span>
+        </div>
+    </div>
+
 </div>
+
 @endsection
